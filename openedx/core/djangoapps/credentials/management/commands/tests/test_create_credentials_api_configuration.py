33e27ec810b1698ac8590e44b20f04bc6e8128da
Thomas Tracy <contact@tjtracy.com>
2021-03-22 16:37:56 -0400

    Fix test case.

--- openedx/core/djangoapps/credentials/management/commands/tests/test_create_credentials_api_configuration.py
+++ openedx/core/djangoapps/credentials/management/commands/tests/test_create_credentials_api_configuration.py
@@ -4,11 +4,15 @@ Tests for the create_credentials_api_configuration command
 
 from unittest import TestCase
 
+import mock
 import pytest
 from django.core.management import call_command
 
 from openedx.core.djangoapps.credentials.models import CredentialsApiConfig
 
+from ..create_credentials_api_configuration import Command
+
+COMMAND_MODULE = 'openedx.core.djangoapps.credentials.management.commands.create_credentials_api_configuration'
 
 @pytest.mark.django_db
 class CertAllowlistGenerationTests(TestCase):
@@ -16,6 +20,7 @@ class CertAllowlistGenerationTests(TestCase):
     Tests for the create_credentials_api_configuration management command
     """
 
-    def test_successful_generation(self):
-        call_command("create_credentials_api_configuration")
+    @mock.patch(COMMAND_MODULE)
+    def test_successful_generation(self, mock):
+        call_command(Command())
         assert len(CredentialsApiConfig.objects.all()) > 0
