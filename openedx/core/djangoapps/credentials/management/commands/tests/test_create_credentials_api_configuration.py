d143a807594b22f17cfe7f91c070535db6099d37
Thomas Tracy <contact@tjtracy.com>
2021-03-22 12:50:18 -0400

    Add unit test. Fix doc string.

---
+++ openedx/core/djangoapps/credentials/management/commands/tests/test_create_credentials_api_configuration.py
@@ -0,0 +1,19 @@
+"""
+Tests for the create_credentials_api_configuration command
+"""
+
+import pytest
+from unittest import TestCase
+from django.core.management import CommandError, call_command
+from waffle.testutils import override_switch
+from openedx.core.djangoapps.credentials.models import CredentialsApiConfig
+
+@pytest.mark.django_db
+class CertAllowlistGenerationTests(TestCase):
+    """
+    Tests for the create_credentials_api_configuration management command
+    """
+
+    def test_successful_generation(self):
+        call_command("create_credentials_api_configuration")
+        assert len(CredentialsApiConfig.objects.all()) > 0
